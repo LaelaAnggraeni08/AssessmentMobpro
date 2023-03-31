fd2bbea939eaed5114a9eaa883c4e6f1e41f29bb
Laela Anggraeni <anggraenilaila64@gmail.com>
2023-03-31 14:12:01 +0700

    Initial commit

--- app/src/main/java/org/d3if3111/assessmentmobpro/MainActivity.kt
+++ app/src/main/java/org/d3if3111/assessmentmobpro/MainActivity.kt
@@ -2,10 +2,42 @@ package org.d3if3111.assessmentmobpro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
+import org.d3if3111.assessmentmobpro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener { hitungRataRata() }
+    }
+
+    private fun hitungRataRata() {
+
+        val nilaiSatu = binding.nilai1Inp.text.toString()
+        if (TextUtils.isEmpty(nilaiSatu)) {
+            Toast.makeText(this, R.string.nilai1_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val nilaiDua = binding.nilai2Inp.text.toString()
+        if (TextUtils.isEmpty(nilaiDua)) {
+            Toast.makeText(this, R.string.nilai2_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val nilaiTiga = binding.nilai1Inp.text.toString()
+        if (TextUtils.isEmpty(nilaiTiga)) {
+            Toast.makeText(this, R.string.nilai3_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val average = (nilaiSatu.toFloat() + nilaiDua.toFloat() + nilaiTiga.toFloat()) / 3
+
+        binding.hasilTextView.text = getString(R.string.hasil, average)
     }
 }
